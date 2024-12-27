c6af185b63f5558d0c52b2e63ff9063fd7b262b1
Tomohiro Yamashina <110611464+vakasurvivor@users.noreply.github.com>
2024-12-27 20:02:48 +0900

    変更

--- src/component/Works/Event.jsx
+++ src/component/Works/Event.jsx
@@ -1,7 +1,7 @@
-import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
 // 使用する gsapプラグイン を登録する
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,30 +11,68 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function Event() {
 	const container = useRef(null);
+
 	useGSAP(
 		() => {
+			const mediaQuery = window.matchMedia('(max-width: 768px)');
 			const wrapper = document.querySelector('.js-event__wrapper');
 			const slides = gsap.utils.toArray('.js-event__scroll');
 			const wrapperWidth = wrapper.offsetWidth;
-			// gsap code here...
-			gsap.to(slides, {
-				xPercent: -100 * (slides.length - 1), // -X軸方向に移動
-				x: 32 * 3,
-				ease: 'none', // アニメーションのイージング(noneは定速)
-				scrollTrigger: {
-					trigger: wrapper, // アニメーション開始のトリガー要素
-					pin: true, // 要素を固定
-					scrub: 1, // スクロール量に合わせてアニメーション
-					start: 'bottom bottom', // アニメーションが始まる位置
-					end: `+=${wrapperWidth}`, // アニメーションが終わる位置
-					anticipatePin: 1, // ピン留めアニメーションをスムーズに開始
-					invalidateOnRefresh: true, // ページの再読み込み時(リサイズ時)に値を再計算する
-					// markers: true,
-				},
-			});
+
+			const animateSlides = () => {
+				if (!mediaQuery.matches) {
+					gsap.to(slides, {
+						xPercent: -100 * (slides.length - 1), // -X軸方向に移動
+						x: 32 * 3,
+						ease: 'none', // アニメーションのイージング(noneは定速)
+						scrollTrigger: {
+							trigger: wrapper, // アニメーション開始のトリガー要素
+							pin: true, // 要素を固定
+							scrub: 1, // スクロール量に合わせてアニメーション
+							start: 'bottom bottom', // アニメーションが始まる位置
+							end: `+=${wrapperWidth}`, // アニメーションが終わる位置
+							anticipatePin: 1, // ピン留めアニメーションをスムーズに開始
+							invalidateOnRefresh: true, // ページの再読み込み時(リサイズ時)に値を再計算する
+							// markers: true,
+						},
+					});
+				}
+			};
+
+			mediaQuery.addEventListener('change', animateSlides);
+			animateSlides(); // 初回実行
+
+			return () => {
+				mediaQuery.removeEventListener('change', animateSlides);
+			};
 		},
 		{ scope: container }
 	);
+
+	// useGSAP(
+	// 	() => {
+	// 		const wrapper = document.querySelector('.js-event__wrapper');
+	// 		const slides = gsap.utils.toArray('.js-event__scroll');
+	// 		const wrapperWidth = wrapper.offsetWidth;
+	// 		// gsap code here...
+	// 		gsap.to(slides, {
+	// 			xPercent: -100 * (slides.length - 1), // -X軸方向に移動
+	// 			x: 32 * 3,
+	// 			ease: 'none', // アニメーションのイージング(noneは定速)
+	// 			scrollTrigger: {
+	// 				trigger: wrapper, // アニメーション開始のトリガー要素
+	// 				pin: true, // 要素を固定
+	// 				scrub: 1, // スクロール量に合わせてアニメーション
+	// 				start: 'bottom bottom', // アニメーションが始まる位置
+	// 				end: `+=${wrapperWidth}`, // アニメーションが終わる位置
+	// 				anticipatePin: 1, // ピン留めアニメーションをスムーズに開始
+	// 				invalidateOnRefresh: true, // ページの再読み込み時(リサイズ時)に値を再計算する
+	// 				// markers: true,
+	// 			},
+	// 		});
+	// 	},
+	// 	{ scope: container }
+	// );
 
 	return (
 		<>
